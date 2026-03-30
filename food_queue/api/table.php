6b2cf97d727bd0ddaf36913255fddb28117d0843
Vanna-study <s6806021411074@email.kmutnb.ac.th>
2026-03-30 11:04:03 +0700

    feat : add REQUEST_METHOD = OPTIONS

--- food_queue/api/table.php
+++ food_queue/api/table.php
@@ -4,6 +4,12 @@ header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
 header("Content-Type: application/json");
 
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
+
+
 require_once '../db.php';
 require_once '../model/table.php';
 
